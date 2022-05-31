f2e79adbfa049bd23267e38aa91d63e5418a1f49
FISHWONGY <59711659+FISHWONGY@users.noreply.github.com>
2022-05-31 21:27:49 +0100

    add script

---
+++ The_Complete_Machine_Learning_Course_with_Python/3_Regression/07_Variance-Bias Tradeoff.py
@@ -0,0 +1,492 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Variance Bias Trade off
+'''
+Every estimator has its advantages and drawbacks. 
+Its generalization error can be decomposed in terms of bias, variance and noise. 
+The bias of an estimator is its average error for different training sets. 
+The variance of an estimator indicates how sensitive it is to varying training sets. 
+Noise is a property of the data.
+
+Bias and variance are inherent properties of estimators and we usually have to select learning algorithms and 
+hyperparameters so that both bias and variance are as low as possible. 
+Another way to reduce the variance of a model is to use more training data. 
+However, you should only collect more training data if the true function is too complex to be 
+approximated by an estimator with a lower variance.
+'''
+
+import sklearn
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score
+
+def true_fun(X):
+    return np.cos(1.5 * np.pi * X)
+
+np.random.seed(0)
+
+n_samples = 30
+degrees = [1, 4, 15]
+
+X = np.sort(np.random.rand(n_samples))
+y = true_fun(X) + np.random.randn(n_samples) * 0.1
+
+plt.figure(figsize=(14, 5))
+for i in range(len(degrees)):
+    ax = plt.subplot(1, len(degrees), i + 1)
+    plt.setp(ax, xticks=(), yticks=())
+
+    polynomial_features = PolynomialFeatures(degree=degrees[i],
+                                             include_bias=False)
+    linear_regression = LinearRegression()
+    pipeline = Pipeline([("polynomial_features", polynomial_features),
+                         ("linear_regression", linear_regression)])
+    pipeline.fit(X[:, np.newaxis], y)
+
+    # Evaluate the models using crossvalidation
+    scores = cross_val_score(pipeline, X[:, np.newaxis], y,
+                             scoring="neg_mean_squared_error", cv=10)
+
+    X_test = np.linspace(0, 1, 100)
+    plt.plot(X_test, pipeline.predict(X_test[:, np.newaxis]), label="Model")
+    plt.plot(X_test, true_fun(X_test), label="True function")
+    plt.scatter(X, y, edgecolor='b', s=20, label="Samples")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.xlim((0, 1))
+    plt.ylim((-2, 2))
+    plt.legend(loc="best")
+    plt.title("Degree {}\nMSE = {:.2e}(+/- {:.2e})".format(
+        degrees[i], -scores.mean(), scores.std()))
+plt.show()
+
+
+# Validation Curve
+'''
+- For identification of over- and under-fitting
+- Plotting training and validation scores vs model parameters
+'''
+
+# Validation Curve for Ridge Regression
+np.logspace(-5, 2, 10)
+
+
+from sklearn.model_selection import validation_curve
+from sklearn.datasets import load_iris
+from sklearn.linear_model import Ridge
+
+np.random.seed(0)
+iris = load_iris()
+X, y = iris.data, iris.target
+indices = np.arange(y.shape[0])
+np.random.shuffle(indices)
+X, y = X[indices], y[indices]
+param_name = "alpha"
+param_range = np.logspace(-5, 2, 10)
+
+train_scores, valid_scores = validation_curve(Ridge(),
+                                              X,
+                                              y,
+                                              param_name,
+                                              param_range,
+                                              scoring='r2',
+                                              cv=5)
+print(train_scores)
+print(valid_scores)
+
+train_mean = np.mean(train_scores, axis=1)
+train_std = np.std(train_scores, axis=1)
+
+test_mean = np.mean(valid_scores, axis=1)
+test_std = np.std(valid_scores, axis=1)
+
+
+plt.figure(figsize=(10,8))
+plt.plot(param_range, train_mean,
+         color='blue', marker='o',
+         markersize=5, label='training accuracy')
+plt.fill_between(param_range, train_mean + train_std,
+                 train_mean - train_std, alpha=0.15,
+                 color='blue')
+plt.plot(param_range, test_mean,
+         color='green', linestyle='--',
+         marker='s', markersize=5,
+         label='validation accuracy')
+plt.fill_between(param_range,
+                 test_mean + test_std,
+                 test_mean - test_std,
+                 alpha=0.15, color='green')
+plt.grid()
+plt.xscale('log')
+plt.legend(loc='lower right')
+plt.xlabel('Parameter alpha')
+plt.ylabel('R2')
+# plt.ylim([0.8, 1.0])
+plt.show()
+
+# Plotting Validation Curve
+'''
+In this plot you can see the training scores and 
+validation scores of an SVM for different values of the kernel parameter gamma.
+
+For very low values of gamma, you can see that both the training score and 
+the validation score are low. This is called underfitting.
+
+Medium values of gamma will result in high values for both scores, 
+i.e. the classifier is performing fairly well.
+
+If gamma is too high, the classifier will overfit, 
+which means that the training score is good but the validation score is poor.
+'''
+
+from sklearn.datasets import load_digits
+from sklearn.svm import SVC
+from sklearn.model_selection import validation_curve
+
+digits = load_digits()
+X, y = digits.data, digits.target
+
+param_range = np.logspace(-6, -1, 10)
+train_scores, test_scores = validation_curve(SVC(), X, y, param_name="gamma", param_range=param_range,
+                                             cv=10, scoring="accuracy", n_jobs=1)
+
+train_scores_mean = np.mean(train_scores, axis=1)
+train_scores_std = np.std(train_scores, axis=1)
+test_scores_mean = np.mean(test_scores, axis=1)
+test_scores_std = np.std(test_scores, axis=1)
+
+
+plt.figure(figsize=(10,8))
+plt.title("Validation Curve with SVM")
+plt.xlabel("$\gamma$")
+plt.ylabel("Score")
+plt.ylim(0.0, 1.1)
+lw = 2
+plt.semilogx(param_range, train_scores_mean, label="Training score",
+             color="darkorange", lw=lw)
+plt.fill_between(param_range, train_scores_mean - train_scores_std,
+                 train_scores_mean + train_scores_std, alpha=0.2,
+                 color="darkorange", lw=lw)
+plt.semilogx(param_range, test_scores_mean, label="Cross-validation score",
+             color="navy", lw=lw)
+plt.fill_between(param_range, test_scores_mean - test_scores_std,
+                 test_scores_mean + test_scores_std, alpha=0.2,
+                 color="navy", lw=lw)
+plt.legend(loc="best")
+plt.show()
+
+
+# Learning Curve
+'''
+- Shows the validation and training score of an estimator for varying numbers of training samples.
+
+- A tool to find out how much we benefit from adding more training data and whether the estimator suffers more from a variance error or a bias error.
+
+- If both the validation score and the training score converge to a value that is too low with increasing size of the training set, we will not benefit much from more training data.
+
+Below is an example of a good variance bias trade off learning curve.
+- The actual level of performance (E.g., Accuracy or 𝑅2 score) achieved is close to desired level of performance
+
+- The training and test (validation curve) are tight and converge to similar level
+
+Below is an example of a High Bias learning curve.
+- Low training and test accuracy / score. I.e., Underfits the training data
+- The actual performance (E.g., Accuracy or 𝑅2 score) level is far from the desired level of performance
+
+Address by:
+- Increase the number of parameters - Adding or creating new feature
+- Decrease regularisation
+
+Below is an example of a High Variance learning curve.
+- The training and test level do not converge or converge slowly
+- The training and test level are still very far apart. I.e., This is an example of overfitting
+
+Address by:
+- Collect more training data
+- Reduce model complexity
+- Increase regularisaiton
+
+
+In summary we look for:
+- Bias
+    - Evaluate via score / accuracy level
+
+- Variance
+    - Evaluate via the convergence speed and distance between 
+    training score / accuracy level and test score / accuracy level
+'''
+
+# Learning Curves Example
+
+'''
+The example below is from scikit-learn documentation website
+
+General observations of learning curves:
+- The training score is very high at the beginning and decreases
+- The cross-validation score is very low at the beginning and increases.
+
+Key observations for Naive Bayes Classifier:
+- Both the training score and the cross-validation score are not very good at the end
+
+Key observation for SVM:
+- The training score is still around the maximum
+- The validation score could be increased with more training samples.
+'''
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import learning_curve
+from sklearn.model_selection import ShuffleSplit
+
+# run line 244 - 334 all tgt
+def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None, n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
+    """
+    Generate a simple plot of the test and training learning curve.
+
+    Parameters
+    ----------
+    estimator : object type that implements the "fit" and "predict" methods
+        An object of that type which is cloned for each validation.
+
+    title : string
+        Title for the chart.
+
+    X : array-like, shape (n_samples, n_features)
+        Training vector, where n_samples is the number of samples and
+        n_features is the number of features.
+
+    y : array-like, shape (n_samples) or (n_samples, n_features), optional
+        Target relative to X for classification or regression;
+        None for unsupervised learning.
+
+    ylim : tuple, shape (ymin, ymax), optional
+        Defines minimum and maximum yvalues plotted.
+
+    cv : int, cross-validation generator or an iterable, optional
+        Determines the cross-validation splitting strategy.
+        Possible inputs for cv are:
+          - None, to use the default 3-fold cross-validation,
+          - integer, to specify the number of folds.
+          - An object to be used as a cross-validation generator.
+          - An iterable yielding train/test splits.
+
+        For integer/None inputs, if ``y`` is binary or multiclass,
+        :class:`StratifiedKFold` used. If the estimator is not a classifier
+        or if ``y`` is neither binary nor multiclass, :class:`KFold` is used.
+
+        Refer :ref:`User Guide <cross_validation>` for the various
+        cross-validators that can be used here.
+
+    n_jobs : integer, optional
+        Number of jobs to run in parallel (default 1).
+    """
+    plt.figure(figsize=(8,6))
+    plt.title(title)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+             label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+             label="Cross-validation score")
+
+    plt.legend(loc="best")
+    return plt
+
+
+digits = load_digits()
+X, y = digits.data, digits.target
+
+
+title = "Learning Curves (Naive Bayes)"
+# Cross validation with 100 iterations to get smoother mean test and train
+# score curves, each time with 20% data randomly selected as a validation set.
+cv = ShuffleSplit(n_splits=100, test_size=0.2, random_state=0)
+
+estimator = GaussianNB()
+plot_learning_curve(estimator, title, X, y,
+                    ylim=(0.7, 1.01),
+                    cv=cv,
+                    n_jobs=4)
+
+title = "Learning Curves (SVM, RBF kernel, $\gamma=0.001$)"
+# SVC is more expensive so we do a lower number of CV iterations:
+cv = ShuffleSplit(n_splits=10, test_size=0.2, random_state=0)
+estimator = SVC(gamma=0.001)
+plot_learning_curve(estimator, title, X, y, (0.7, 1.01), cv=cv, n_jobs=4)
+
+plt.show()
+
+
+# Run line 339 - 406 all tgt
+# Exercise - Boston Housing
+def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
+                        n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
+    """
+    Generate a simple plot of the test and training learning curve.
+
+    Parameters
+    ----------
+    estimator : object type that implements the "fit" and "predict" methods
+        An object of that type which is cloned for each validation.
+
+    title : string
+        Title for the chart.
+
+    X : array-like, shape (n_samples, n_features)
+        Training vector, where n_samples is the number of samples and
+        n_features is the number of features.
+
+    y : array-like, shape (n_samples) or (n_samples, n_features), optional
+        Target relative to X for classification or regression;
+        None for unsupervised learning.
+
+    ylim : tuple, shape (ymin, ymax), optional
+        Defines minimum and maximum yvalues plotted.
+
+    cv : int, cross-validation generator or an iterable, optional
+        Determines the cross-validation splitting strategy.
+        Possible inputs for cv are:
+          - None, to use the default 3-fold cross-validation,
+          - integer, to specify the number of folds.
+          - An object to be used as a cross-validation generator.
+          - An iterable yielding train/test splits.
+
+        For integer/None inputs, if ``y`` is binary or multiclass,
+        :class:`StratifiedKFold` used. If the estimator is not a classifier
+        or if ``y`` is neither binary nor multiclass, :class:`KFold` is used.
+
+        Refer :ref:`User Guide <cross_validation>` for the various
+        cross-validators that can be used here.
+
+    n_jobs : integer, optional
+        Number of jobs to run in parallel (default 1).
+    """
+    plt.figure(figsize=(10, 8))
+    plt.title(title)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+             label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+             label="Cross-validation score")
+
+    plt.legend(loc="best")
+    return plt
+
+
+from sklearn.datasets import load_boston
+boston = load_boston()
+X, y = boston.data, boston.target
+
+title = "Learning Curves (Ridge Regression)"
+
+# Cross validation with 100 iterations to get smoother mean test and train
+# score curves, each time with 20% data randomly selected as a validation set.
+
+cv = ShuffleSplit(n_splits=100, test_size=0.2, random_state=0)
+estimator = Ridge()
+plot_learning_curve(estimator, title, X, y, cv=cv, n_jobs=4)
+plt.show()
+
+
+# Validation Curve
+np.random.seed(0)
+boston = load_boston()
+X, y = boston.data, boston.target
+indices = np.arange(y.shape[0])
+np.random.shuffle(indices)
+X, y = X[indices], y[indices]
+param_name = "alpha"
+param_range = np.logspace(-5, 2, 10)
+
+train_scores, valid_scores = validation_curve(Ridge(),
+                                              X,
+                                              y,
+                                              param_name,
+                                              param_range)
+print(train_scores)
+print(valid_scores)
+
+train_mean = np.mean(train_scores, axis=1)
+train_std = np.std(train_scores, axis=1)
+
+test_mean = np.mean(valid_scores, axis=1)
+test_std = np.std(valid_scores, axis=1)
+
+# Run line 449 - 470 all tgt
+plt.figure(figsize=(10, 8))
+plt.plot(param_range, train_mean,
+         color='blue', marker='o',
+         markersize=5, label='training accuracy')
+plt.fill_between(param_range, train_mean + train_std,
+                 train_mean - train_std, alpha=0.15,
+                 color='blue')
+plt.plot(param_range, test_mean,
+         color='green', linestyle='--',
+         marker='s', markersize=5,
+         label='validation accuracy')
+plt.fill_between(param_range,
+                 test_mean + test_std,
+                 test_mean - test_std,
+                 alpha=0.15, color='green')
+plt.grid()
+plt.xscale('log')
+plt.legend(loc='lower right')
+plt.xlabel('Parameter alpha')
+plt.ylabel('Accuracy')
+#plt.ylim([0.8, 1.0])
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ The_Complete_Machine_Learning_Course_with_Python/3_Regression/08_Cross Validation.py
@@ -0,0 +1,200 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style("whitegrid")
+
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+from sklearn import svm
+
+boston = datasets.load_boston()
+
+# Cross Validation (CV)
+'''
+- Hold out Cross Validation
+- k-fold Cross Validation
+
+A test set should still be held out for final evaluation, but the validation set is no longer needed when doing CV.
+
+In the basic approach, called k-fold CV, the training set is split into k smaller sets. 
+The following procedure is followed for each of the k “folds”:
+- A model is trained using k-1 of the folds as training data;
+- the resulting model is validated on the remaining part of the data (i.e., it is used as a test set to compute a 
+performance measure such as accuracy).
+
+The performance measure reported by k-fold cross-validation is then the average of the values computed in the loop.
+'''
+
+# Holdout Method
+'''
+- Split initial dataset into a separate training and test dataset
+- Training dataset - model training
+- Test dataset - estimate its generalisation performance
+
+A variation is to split the training set to two :- training set and validation set
+
+Training set:- For fitting different models
+Validation set :- For tuning and comparing different parameter settings to further improve the 
+performance for making predictions on unseen data. And finally for model selection.
+
+This process is called model selection. We want to select the optimal values of tuning parameters (also called hyperparameters).
+'''
+
+# K-fold Cross-validation
+'''
+- Randomly split the training dataset into k folds without replacement.
+- k — 1 folds are used for the model training.
+- The one fold is used for performance evaluation.
+
+This procedure is repeated k times.
+
+Final outcomes:- k models and performance estimates.
+
+- calculate the average performance of the models based on the different, 
+independent folds to obtain a performance estimate that is less sensitive to the sub-partitioning of the training data 
+compared to the holdout method.
+
+- k-fold cross-validation is used for model tuning. Finding the optimal hyperparameter values that yields a satisfying 
+generalization performance.
+
+- Once we have found satisfactory hyperparameter values, we can retrain the model on the complete training set and 
+obtain a final performance estimate using the independent test set. The rationale behind fitting a model to the whole 
+training dataset after k-fold cross-validation is that providing more training samples to a learning algorithm usually 
+results in a more accurate and robust model.
+
+- Common k is 10
+
+- For relatively small training sets, increase the number of folds.
+'''
+
+# Stratified k-fold cross-validation
+'''
+- variation of k-fold
+- Can yield better bias and variance estimates, especially in cases of unequal class proportions
+'''
+
+
+# Cross-validation: evaluating estimator performance
+X_train, X_test, y_train, y_test = train_test_split(boston.data, boston.target, test_size=0.4, random_state=0)
+
+print(X_train.shape, y_train.shape)
+
+print(X_test.shape, y_test.shape)
+
+regression = svm.SVR(kernel='linear', C=1).fit(X_train, y_train)
+regression.score(X_test, y_test)
+
+'''
+When evaluating different settings (“hyperparameters”) for estimators, such as the C setting that must be manually set 
+for an SVM, there is still a risk of overfitting on the test set because the parameters can be tweaked until 
+the estimator performs optimally.
+
+This way, knowledge about the test set can “leak” into the model and evaluation metrics no longer report on generalization performance.
+
+To solve this problem, yet another part of the dataset can be held out as a so-called “validation set”: 
+training proceeds on the training set, after which evaluation is done on the validation set, 
+and when the experiment seems to be successful, final evaluation can be done on the test set.
+
+However, by partitioning the available data into three sets, we drastically reduce the number of samples which can be 
+used for learning the model, and the results can depend on a particular random choice for the pair of (train, validation) sets.
+
+A solution to this problem, as discussed earlier, is a procedure called cross-validation (CV for short). 
+A test set should still be held out for final evaluation, but the validation set is no longer needed when doing CV. 
+In the basic approach, called k-fold CV, the training set is split into k smaller sets 
+(other approaches are described below, but generally follow the same principles). 
+The following procedure is followed for each of the k “folds”:
+
+- A model is trained using k-1 of the folds as training data;
+- the resulting model is validated on the remaining part of the data 
+ (i.e., it is used as a test set to compute a performance measure such as accuracy).
+
+The performance measure reported by k-fold cross-validation is then the average of the values computed in the loop. 
+This approach can be computationally expensive, but does not waste too much data 
+(as it is the case when fixing an arbitrary test set), 
+which is a major advantage in problem such as inverse inference where the number of samples is very small.
+'''
+
+# Computing cross-validated metrics
+from sklearn.model_selection import cross_val_score
+regression = svm.SVR(kernel='linear', C=1)
+scores = cross_val_score(regression, boston.data, boston.target, cv=5)
+scores
+
+# The mean score and the 95% confidence interval of the score estimate are hence given by:
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std()))
+
+# By default, the score computed at each CV iteration is the score method of the estimator.
+# It is possible to change this by using the scoring parameter:
+
+scores = cross_val_score(regression, boston.data, boston.target, cv=5, scoring='neg_mean_squared_error')
+scores
+
+
+# K-fold
+'''
+KFold divides all the samples in k groups of samples, called folds 
+(if k = n, this is equivalent to the Leave One Out strategy), of equal sizes (if possible). 
+The prediction function is learned using k - 1 folds, and the fold left out is used for test.
+
+Example of 2-fold cross-validation on a dataset with 4 samples:
+'''
+
+from sklearn.model_selection import KFold
+
+X = ["a", "b", "c", "d"]
+kf = KFold(n_splits=2)
+for train, test in kf.split(X):
+    print("%s %s" % (train, test))
+
+
+# Stratified k-fold
+'''
+StratifiedKFold is a variation of k-fold which returns stratified folds: each set contains approximately 
+the same percentage of samples of each target class as the complete set.
+
+Example of stratified 3-fold cross-validation on a dataset with 10 samples from two slightly unbalanced classes
+'''
+from sklearn.model_selection import StratifiedKFold
+
+X = np.ones(10)
+y = [0, 0, 0, 0, 1, 1, 1, 1, 1, 1]
+skf = StratifiedKFold(n_splits=3)
+for train, test in skf.split(X, y):
+    print("%s %s" % (train, test))
+
+print(X)
+print(y)
+
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.pipeline import make_pipeline
+pipe_svm = make_pipeline(StandardScaler(),
+                         PCA(n_components=2),
+                         svm.SVR(kernel='linear', C=1))
+pipe_svm.fit(X_train, y_train)
+y_pred = pipe_svm.predict(X_test)
+print('Test Accuracy: %.3f' % pipe_svm.score(X_test, y_test))
+
+
+scores = cross_val_score(estimator=pipe_svm,
+                         X=X_train,
+                         y=y_train,
+                         cv=10,
+                         n_jobs=1)
+print('CV accuracy scores: %s' % scores)
+
+print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
+
+
+
+
+
+
+
+
+
+
+
